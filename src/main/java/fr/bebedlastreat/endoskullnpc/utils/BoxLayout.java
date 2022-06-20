f3b8eb545d92db7780f497fbc6800444f29a5aa6
BebeDlaStreat <bebe.vandroux@gmail.com>
2022-06-20 15:50:12 +0200

    patch box layout 2

--- src/main/java/fr/bebedlastreat/endoskullnpc/utils/BoxLayout.java
+++ src/main/java/fr/bebedlastreat/endoskullnpc/utils/BoxLayout.java
@@ -19,8 +19,13 @@ public class BoxLayout implements LayoutExtension {
         if (layout.getName().equals(LAYOUT_ULTIME)) {
             System.out.println("Layout chargé !");
             layout.registerTextProcessor((player, interaction, text) -> {
-                return text.replaceAll("%key_ultime%", Languages.getLang(player).getMessage(LobbyMessage.KEY_ULTIME))
-                        .replaceAll("%key_vote%", Languages.getLang(player).getMessage(LobbyMessage.KEY_VOTE));
+                return text.replaceAll("%key_ultime%", Languages.getLang(player).getMessage(LobbyMessage.KEY_ULTIME));
+            });
+        }
+        if (layout.getName().equals(LAYOUT_VOTE)) {
+            System.out.println("Layout chargé !");
+            layout.registerTextProcessor((player, interaction, text) -> {
+                return text.replaceAll("%key_vote%", Languages.getLang(player).getMessage(LobbyMessage.KEY_VOTE));
             });
         }
     }
